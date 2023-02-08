8a3323612e1be033d474f9f54aa2ccdaa51272ff
Egemen Nair <nairegemen@gmail.com>
2023-02-08 08:28:22 +0300

    Basic adjustments on .ejs files and app.js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'))
+
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Index Page");
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    page_name: 'index'
+  });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about'
+  });
 });
 
 app.listen(port, () => {
